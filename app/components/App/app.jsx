56b233bab82b8c43cacc1d847876808bd24a544e
James Conkling <james.lane.conkling@gmail.com>
2017-10-21 18:04:22 -0400

    add spinner for loading state

--- app/components/App/app.jsx
+++ app/components/App/app.jsx
@@ -1,17 +1,31 @@
 import React       from 'react';
 import Header      from '../Header';
+import Spinner     from '../Spinner';
+import                  './style';
 
 
 export default ({ status, currentWeather, hourlyWeather }) => (
   <div className="app">
     <Header />
     <div className="limiter">
-      <div>{status}</div>
-      <div>
-        <pre>{JSON.stringify(currentWeather)}</pre>
-      </div>
-      <div>
-        <pre>{JSON.stringify(hourlyWeather)}</pre>
+      <div className="body-container">
+        {status === 'pending' &&
+          <div className="spinner-container">
+            <Spinner />
+          </div>
+        }
+        {status === 'complete' &&
+          <div className="error">
+            <h2>Error</h2>
+            <p>There was an error contacting the server.  Please check your internet connection and try again.</p>
+          </div>
+        }
+        {status === 'completex' &&
+          <div>
+            <pre>{JSON.stringify(currentWeather, null, 2)}</pre>
+            <pre>{JSON.stringify(hourlyWeather, null, 2)}</pre>
+          </div>
+        }
       </div>
     </div>
   </div>
---
+++ app/components/Spinner/spinner.jsx
@@ -0,0 +1,16 @@
+import React                   from 'react';
+import {}                      from 'prop-types';
+import                              './style.scss';
+
+
+const Spinner = () => (
+  <div
+    className="spinner"
+  >
+  </div>
+);
+
+
+Spinner.propTypes = {};
+
+export default Spinner;
